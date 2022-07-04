44c1f31cb3649556f5ed69b0532711e6bbb46457
@roth-dev <roth.dev.ops@gmail.com>
2022-07-04 21:15:33 +0700

    change name

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
-import sketch from './sketch';
-import p5 from 'p5';
+import React from "react";
+import "./App.css";
+import sketch from "./sketch";
+import p5 from "p5";
 
-const TEXT = 'Happy Birthday Trisha';
+const TEXT = "Happy Birthday Phai";
 
 const App = () => {
-  const canvasRef = React.useRef(null)
+  const canvasRef = React.useRef(null);
 
   React.useEffect(() => {
     const canvasDivElement = canvasRef.current;
@@ -14,13 +14,13 @@ const App = () => {
     // NOTE: If you change TEXT value to something else, you'll need to make changes in sketch.js as well to make it work
     // See my comments in sketch.js
     new p5(sketch(canvasDivElement, TEXT), canvasDivElement);
-  })
+  });
 
   return (
     <div className="main">
       <div ref={canvasRef} />
-    </div >
-  )
-}
+    </div>
+  );
+};
 
 export default App;
--- src/sketch/sketch.js
+++ src/sketch/sketch.js
@@ -1,8 +1,7 @@
-import Dot from './Dot';
-import fontFile from './AvenirNextLTPro-Demi.otf';
+import Dot from "./Dot";
+import fontFile from "./AvenirNextLTPro-Demi.otf";
 
-const defaultFrameRate = 30; // low framerate to avoid too much strain on mobile phones
-
+const defaultFrameRate = 25; // low framerate to avoid too much strain on mobile phones
 
 export default (parent, text) => (sketch) => {
   let font;
@@ -10,6 +9,7 @@ export default (parent, text) => (sketch) => {
   let firstWordPoints;
   let secondWordPoints;
   let thirdWordPoints = [];
+  let fourWordPoints = [];
 
   sketch.preload = () => {
     font = sketch.loadFont(fontFile);
@@ -17,24 +17,56 @@ export default (parent, text) => (sketch) => {
 
   const fillDots = (width, height) => {
     dots = [];
-    let [firstWord, secondWord, thirdWord] = text.split(' ') // Assumption that text is just 3 words, no more, no less
+    let [firstWord, secondWord, thirdWord] = text.split(" "); // Assumption that text is just 3 words, no more, no less
 
     // A dirty hack to make it work on both desktop and mobile phones
     if (width > height) {
       // console.log('desktop view')
 
       // ref: https://p5js.org/reference/#/p5.Font/textToPoints
-      // I just tweaked these numbers until it "looked right", mainly trial and error :D 
-      // The numbers would be different for different text, any suggestions about better way to do this are welcome :) 
-      firstWordPoints = font.textToPoints(`${firstWord} ${secondWord}`, width * 0.04, height * 0.33, width * 0.12)
-      secondWordPoints = font.textToPoints(thirdWord, width * 0.05, height * 0.83, width * 0.3);
+      // I just tweaked these numbers until it "looked right", mainly trial and error :D
+      // The numbers would be different for different text, any suggestions about better way to do this are welcome :)
+      firstWordPoints = font.textToPoints(
+        `${firstWord} ${secondWord}`,
+        width * 0.04,
+        height * 0.33,
+        width * 0.12
+      );
+      secondWordPoints = font.textToPoints(
+        thirdWord,
+        width * 0.05,
+        height * 0.83,
+        width * 0.3
+      );
     } else {
       // console.log('in mobile view')
 
-      firstWordPoints = font.textToPoints(firstWord, width * 0.01, height * 0.3, width * 0.3);
-      secondWordPoints = font.textToPoints(secondWord, width * 0.01, height * 0.5, width * 0.25);
-      thirdWordPoints = font.textToPoints(thirdWord, width * 0.01, height * 0.7, width * 0.32);
+      firstWordPoints = font.textToPoints(
+        firstWord,
+        width * 0.01,
+        height * 0.3,
+        width * 0.3
+      );
+      secondWordPoints = font.textToPoints(
+        secondWord,
+        width * 0.01,
+        height * 0.5,
+        width * 0.25
+      );
+      thirdWordPoints = font.textToPoints(
+        thirdWord,
+        width * 0.01,
+        height * 0.7,
+        width * 0.32
+      );
     }
+
+    // fourWordPoints = font.textToPoints(
+    //   fourWord,
+    //   width * 0.01,
+    //   height * 0.9,
+    //   width * 0.32
+    // );
 
     firstWordPoints.forEach((point) => {
       dots.push(new Dot(point.x, point.y, sketch));
@@ -47,6 +79,10 @@ export default (parent, text) => (sketch) => {
     thirdWordPoints.forEach((point) => {
       dots.push(new Dot(point.x, point.y, sketch));
     });
+
+    // fourWordPoints.forEach((point) => {
+    //   dots.push(new Dot(point.x, point.y, sketch));
+    // });
   };
 
   sketch.setup = () => {
